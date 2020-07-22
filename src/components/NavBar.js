d597ee79cecbf55b9f6fa2a730a0dcb4dd41e654
omid1373 <omiddadvar94@gmail.com>
2020-07-22 17:24:28 +0430

    commit by omid

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,6 @@ class NavBar extends React.Component{
         if(localStorage.getItem("token"))
             this.props.login();
     }
-
     logout = () => {
         localStorage.clear();
         this.props.logout();
