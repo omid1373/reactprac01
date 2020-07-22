4e3eb0ecacac94b17bb7bad9673a896e8d693f6e
omid1373 <omiddadvar94@gmail.com>
2020-07-22 17:18:01 +0430

    commit by omid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {Component} from 'react';
 import './App.css';
 import {BrowserRouter , Route, Switch} from "react-router-dom";
 import Home from './components/Home';
@@ -7,20 +7,25 @@ import Contact from './components/Contact';
 import Login from './components/Login';
 import About from "./components/About";
 
-function App() {
-  return (
-      <BrowserRouter>
-        <div className="App">
-          <NavBar/>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/contact" component={Contact}/>
-            <Route path="/about" component={About}/>
-          </Switch>
-        </div>
-      </BrowserRouter>
-  );
-}
+class App extends Component{
+    componentDidMount() {
 
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <NavBar/>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/contact" component={Contact}/>
+                        <Route path="/about" component={About}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
 export default App;
